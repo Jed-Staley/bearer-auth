219f8d2b1a681bfd0acd0119f717f59199fbf2bd
Jed Staley <JNStaley1@gmail.com>
2024-05-09 23:13:23 -0400

    Finished lab

--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -6,13 +6,14 @@ module.exports = async (req, res, next) => {
 
   try {
 
-    if (!req.headers.authorization) { next('Invalid Login') }
+    if (!req.headers.authorization) { console.log('NEXT'); return res.status(401).send('No authorization header provided'); }
 
+    console.log(req.headers.authorization);
     const token = req.headers.authorization.split(' ').pop();
     const validUser = await users.authenticateWithToken(token);
 
     req.user = validUser;
-    req.token = validUser.token;
+    next();
 
   } catch (e) {
     console.error(e);
--- src/auth/models/users.js
+++ src/auth/models/users.js
@@ -1,6 +1,12 @@
 'use strict';
 
+require('dotenv').config();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.SECRET;
+
+const user = { username: 'admin' };  // TEST user object
+const token = jwt.sign(user, process.env.SECRET);
 
 const userSchema = (sequelize, DataTypes) => {
   const model = sequelize.define('User', {
@@ -9,29 +15,39 @@ const userSchema = (sequelize, DataTypes) => {
     token: {
       type: DataTypes.VIRTUAL,
       get() {
-        return jwt.sign({ username: this.username });
+        return jwt.sign({ username: this.username }, secretKey);
       }
     }
   });
 
   model.beforeCreate(async (user) => {
-    let hashedPass = bcrypt.hash(user.password, 10);
-    user.password = hashedPass;
+    user.password = await bcrypt.hash(user.password, 10);
   });
 
-  // Basic AUTH: Validating strings (username, password) 
-  model.authenticateBasic = async function (username, password) {
-    const user = await this.findOne({ username })
-    const valid = await bcrypt.compare(password, user.password)
-    if (valid) { return user; }
+  model.authenticateBasic = async function(username, password) {
+    const user = await this.findOne({ 
+      where: { username } 
+    });
+
+    if (user) {
+      const valid = await bcrypt.compare(password, user.password);
+      if (valid) {
+        return user;
+      }
+    }
+
     throw new Error('Invalid User');
-  }
+  };
 
   // Bearer AUTH: Validating a token
-  model.authenticateToken = async function (token) {
+  model.authenticateWithToken = async function (token) {
     try {
-      const parsedToken = jwt.verify(token, process.env.SECRET);
-      const user = this.findOne({ username: parsedToken.username })
+      const parsedToken = jwt.verify(token, secretKey);
+      console.log('searching', parsedToken.username)
+      const user = await this.findOne({ 
+        where: { username: parsedToken.username }
+      });
+
       if (user) { return user; }
       throw new Error("User Not Found");
     } catch (e) {
